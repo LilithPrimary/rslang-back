68516f7f6e2acd65f1187f8230920cd8a31e1b03
Denna <lilithprimary@gmail.com>
2022-11-27 18:54:10 +0100

    fix: und problem

--- src/server.js
+++ src/server.js
@@ -9,21 +9,23 @@ const mongoose = require('mongoose');
 const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
 const app = require('./app');
 
-mongoose.connect(MONGO_CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+if (MONGO_CONNECTION_STRING) {
+  mongoose.connect(MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
 
-const db = mongoose.connection;
+  const db = mongoose.connection;
 
-db.on('error', () => logger.error('MongoDB connection error:')).once(
-  'open',
-  () => {
-    logger.info('Successfully connect to DB');
-    app.listen(PORT, () =>
-      logger.info(`App is running on http://localhost:${PORT}`)
-    );
-  }
-);
+  db.on('error', () => logger.error('MongoDB connection error:')).once(
+    'open',
+    () => {
+      logger.info('Successfully connect to DB');
+      app.listen(PORT, () =>
+        logger.info(`App is running on http://localhost:${PORT}`)
+      );
+    }
+  );
+}
